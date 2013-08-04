dbad4011efb8f61a95576d83e8c90cf09fb65cd9
Dylan Ellington <support@dylanellington.com>
2013-08-04 14:20:50 -0400

    Started Bluetooth Implementation

---
+++ tictactoe/BluetoothGameViewController.h
@@ -0,0 +1,54 @@
+//
+//  BluetoothGameViewController.h
+//  tictactoe
+//
+//  Created by Dylan Ellington on 8/2/13.
+//  Copyright (c) 2013 Joshua Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GameKit/GameKit.h>
+
+typedef enum Datatypes
+{
+    Player,
+    Move
+}
+    Datatype;
+
+typedef enum GameButtons
+{
+    ButtonZero, ButtonOne, ButtonTwo,
+    ButtonThree,ButtonFour,ButtonFive,
+    ButtonSix, ButtonSeven,ButtonEight,
+}
+    GameButton;
+
+@interface BluetoothGameViewController : UIViewController <GKSessionDelegate, GKPeerPickerControllerDelegate, NSCoding>
+{
+    GKSession *session;
+    IBOutlet UIButton *disconnect;
+    Datatype type;
+    GameButton button;
+}
+
+@property (nonatomic, retain) GKSession *session;
+@property (nonatomic, retain) UIButton *disconnect;
+@property (nonatomic, copy) NSString *peerID;
+@property (nonatomic) Datatype type;
+@property (nonatomic) GameButton button;
+
+@property (weak, nonatomic) IBOutlet UILabel *infoLabel;
+@property (weak, nonatomic) IBOutlet UIButton *button0;
+@property (weak, nonatomic) IBOutlet UIButton *button1;
+@property (weak, nonatomic) IBOutlet UIButton *button2;
+@property (weak, nonatomic) IBOutlet UIButton *button3;
+@property (weak, nonatomic) IBOutlet UIButton *button4;
+@property (weak, nonatomic) IBOutlet UIButton *button5;
+@property (weak, nonatomic) IBOutlet UIButton *button6;
+@property (weak, nonatomic) IBOutlet UIButton *button7;
+@property (weak, nonatomic) IBOutlet UIButton *button8;
+
+-(IBAction) disconnect:(id) sender;
+
+@end
