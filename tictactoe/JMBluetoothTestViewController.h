41d15933d582996d9cfc8eae71b2bb24dc66f903
Joshua Martin <josh.martin.hhsib@gmail.com>
2013-08-14 01:30:04 -0400

    added bluetooth testing

---
+++ tictactoe/JMBluetoothTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMBluetoothTestViewController.h
+//  tictactoe
+//
+//  Created by Joshua Martin on 8/14/13.
+//  Copyright (c) 2013 Joshua Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMBluetoothTestViewController : UIViewController
+
+@end
---
+++ tictactoe/JMBluetoothTestViewController.m
@@ -0,0 +1,38 @@
+//
+//  JMBluetoothTestViewController.m
+//  tictactoe
+//
+//  Created by Joshua Martin on 8/14/13.
+//  Copyright (c) 2013 Joshua Martin. All rights reserved.
+//
+
+#import "JMBluetoothTestViewController.h"
+
+@interface JMBluetoothTestViewController ()
+
+@end
+
+@implementation JMBluetoothTestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
